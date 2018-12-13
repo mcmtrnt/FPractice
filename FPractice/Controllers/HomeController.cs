5ab49ee976b6acffd91df7308654bd5957424cbb
mcmtrnt <mcmtrnt3@gmail.com>
2018-12-12 17:29:32 -0700

    test

---
+++ FPractice/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //real test
+            return View();
+        }
+    }
+}
